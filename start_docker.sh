f06c6e49b7dede78cb9e79840414cf37084d1235
Daniel Chalef <daniel.chalef@gmail.com>
2015-05-18 16:12:00 -0400

    fixed Nvidia dev string

--- start_docker.sh
+++ start_docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-DOCKER_NVIDIA_DEVICES="--device /dev/nvidia0:/dev/nvidia0 --device /dev/nvidia1:/dev/nvidia1 --device /dev/nvidia1:/dev/nvidia2 --device /dev/nvidia3:/dev/nvidia3--device /dev/nvidiactl:/dev/nvidiactl --device /dev/nvidia-uvm:/dev/nvidia-uvm"
+DOCKER_NVIDIA_DEVICES="--device /dev/nvidia0:/dev/nvidia0 --device /dev/nvidia1:/dev/nvidia1 --device /dev/nvidia2:/dev/nvidia2 --device /dev/nvidia3:/dev/nvidia3 --device /dev/nvidiactl:/dev/nvidiactl --device /dev/nvidia-uvm:/dev/nvidia-uvm"
 docker run -ti -p 8888:8888 -e IP=0.0.0.0 --name scala-cuda -v /home/daniel/analytics:/data $DOCKER_NVIDIA_DEVICES danielchalef/scala-cuda
 
